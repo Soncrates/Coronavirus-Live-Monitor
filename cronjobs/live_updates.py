ef8ad3eaf398461d235d2c7439ceb702ea481059
Soncrates <emersoncus@yahoo.com>
2020-03-26 23:52:21 -0400

    Update live_updates.py

--- cronjobs/live_updates.py
+++ cronjobs/live_updates.py
@@ -15,17 +15,18 @@ from time import gmtime, strftime, sleep
 # function to be run every 10 minutes
 def update_current_cases():
     # send initial request to local Node.js server
-    request = requests.get("http://localhost:7001/")
-    if(request.status_code == 200):
-        # send response (as GET variable) of that request to the PHP server
-        finalRequest = requests.get("https://covid19.xtrp.io/server/get_live_updates.php?src=" + request.text)
-        if(finalRequest.status_code == 200):
-            print("Success " + request.text)
-        else:
-            print("ERROR: " + str(finalRequest.status_code))
-    else:
-        print("ERROR: " + str(request.status_code))
-
+    ret = "http://localhost:7001/"
+    ret = requests.get(ret)
+    if(ret.status_code != 200):
+        print("ERROR: " + str(ret.status_code))
+        return 1
+    # send response (as GET variable) of that request to the PHP server
+    finalRequest = requests.get("https://covid19.xtrp.io/server/get_live_updates.php?src=" + ret.text)
+    if(finalRequest.status_code != 200):
+        print("ERROR: " + str(finalRequest.status_code))
+        return 1
+    print("Success " + request.text)
+    return 0
 # run function
 update_current_cases()
 
